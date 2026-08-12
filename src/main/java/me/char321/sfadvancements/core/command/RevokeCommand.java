22b9effe519d5794664bb24840acda069263397a
wickidcow <jaywago@gmail.com>
2026-08-12 11:31:02 -0400

    Translate revoke command to English

--- src/main/java/me/char321/sfadvancements/core/command/RevokeCommand.java
+++ src/main/java/me/char321/sfadvancements/core/command/RevokeCommand.java
@@ -18,13 +18,13 @@ public class RevokeCommand implements SubCommand {
     @Override
     public boolean onExecute(CommandSender sender, Command command, String label, String[] args) {
         if (args.length < 3) {
-            sender.sendMessage(ChatColor.RED + "用法: /" + label + " revoke <玩家> <进度>");
+            sender.sendMessage(ChatColor.RED + "Usage: /" + label + " revoke <player> <advancement>");
             return false;
         }
 
         Player p = Bukkit.getPlayer(args[1]);
         if (p == null) {
-            sender.sendMessage(ChatColor.RED + "无法找到玩家 " + args[1]);
+            sender.sendMessage(ChatColor.RED + "Could not find player " + args[1]);
             return false;
         }
 
@@ -33,15 +33,15 @@ public class RevokeCommand implements SubCommand {
             for (NamespacedKey adv : progress.getCompletedAdvancements()) {
                 progress.revokeAdvancement(adv);
             }
-            sender.sendMessage("已清除玩家的所有进度!");
+            sender.sendMessage("Revoked all advancements from the player!");
             return true;
         }
 
         if (!progress.revokeAdvancement(NamespacedKey.fromString(args[2]))) {
-            sender.sendMessage(ChatColor.RED + "无法清除玩家 " + args[1] + " 的进度 " + args[2]);
+            sender.sendMessage(ChatColor.RED + "Could not revoke advancement " + args[2] + " from player " + args[1]);
             return false;
         } else {
-            sender.sendMessage("已成功清除该进度!");
+            sender.sendMessage("Successfully revoked the advancement!");
             return true;
         }
     }
